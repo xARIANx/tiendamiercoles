86cc74daf3a6cd6442799f323b7b12a4a9ae0018
ArianCastro <arianchelo19@gmail.com>
2022-02-22 23:43:44 -0500

    Se crea controlador

---
+++ js/controladorTienda.js
@@ -0,0 +1,24 @@
+//ENTRADAS==VARIABLES==CONSTANTES
+let nombreProducto="CD Wu Tang-Clan"
+let precioProducto=400000
+let pesoProducto=18.5
+let tieneDescuento=true
+let descripcionProducto="CD Album Cream Wu Tang-Clan"
+let telefonoVendedor="3023303552"
+
+//CONSTANTES siempre en mayusculas
+const NOMBRE_CLIENTE="Arian"
+
+//ARREGLOS UNIDIMENSIONALES VECTORES
+//Sirven para almacenar varios elementos en 1 sola variable
+let productos=["CD Wu Tang-Clan","Camisa NWA","Cuadro Hip Hop"]
+let productos2=Array("CD Wu Tang-Clan","Camisa NWA","Cuadro Hip Hop")
+
+
+//SALIDAS
+console.log(nombreProducto)
+console.log("Su nombre es: "+NOMBRE_CLIENTE+"Y su producto es: "+nombreProducto)
+console.log(`Su nombre es: ${NOMBRE_CLIENTE} y su producto es: ${nombreProducto}`)
+console.log("******")
+console.log(productos)
+console.log(productos2[2])
